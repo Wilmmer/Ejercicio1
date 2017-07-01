56fa5e920607a6eb13ae99751f8a53b8e1af8664
LAPTOP-J2LNH6VD\Lenovo <wilmmer_052@live.com.mx>
2017-07-01 17:03:28 -0500

    Introducción a C#

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -10,6 +10,93 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+
+            // Ejercicio 1 CalcularSueldo
+            //Declaración de variables
+
+            //int ht;
+            //double sxh;
+            //double S;
+
+            //Solicitar datos
+            //Console.WriteLine("Ingresa las horas:");
+            //ht = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Inresa el sueldo por hora");
+            //sxh = Double.Parse(Console.ReadLine());
+
+            //Determinar si los valores ingresados son validos
+            //if ((ht > 0) && (sxh > 0))
+            //{
+            //    si son validos
+            //   S = ht * sxh;
+
+            //    Console.WriteLine("El sueldo es: " + S.ToString());
+
+            //}
+
+            //else
+            //{         //no son incorrectos
+            //    if (ht < 0)
+            //    {
+            //        Console.WriteLine("Error en ht");
+
+            //    }
+            //    if (sxh < 0)
+            //    {
+            //        Console.WriteLine("Error en sxh");
+            //    }
+            //}
+
+            //Console.WriteLine("Presiona una tecla");
+            //Console.ReadKey();
+
+
+
+
+            // Ejercicio 2 NumMayor-NumMenor
+            //int A;
+            //int B;
+            //int C;
+
+            //Console.WriteLine("Ingresa el primer valor A: ");
+            //A = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Ingresa el segundo valor B: ");
+            //B = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Ingresa el tercer valor C: ");
+            //C = Int32.Parse(Console.ReadLine());
+
+            //    if((A>B) &&(A>C))
+            //    {
+            //        Console.WriteLine("El valor A :" + A.ToString()+" es mas grande");
+            //    }
+            //    if((B>A) && (B>C))
+            //    {
+            //        Console.WriteLine("El valor B :" + B.ToString() + " es mas grande");
+            //    }
+            //    if(C>A && C>B)
+            //    {
+            //        Console.WriteLine("El valor C :" + C.ToString() + " es mas grande");
+            //    }
+
+
+            //    if((A<B) && (A<C))
+            //    {
+            //        Console.WriteLine("El valor A :" + A.ToString() + " es mas pequeño");
+            //    }
+            //    if ((B < A) && (B < C))
+            //    {
+            //        Console.WriteLine("El valor B :" + B.ToString() + " es mas pequeño");
+            //    }
+            //    if (C < A && C < B)
+            //    {
+            //        Console.WriteLine("El valor C :" + C.ToString() + " es mas pequeño");
+            //    }
+
+            //Console.WriteLine("Presiona una tecla");
+            //Console.ReadKey();
         }
     }
 }
